1d7caa017abe1e9ddfe1ad43d84992607fc9b75c
Jsr <jang121a@gmail.com>
2026-04-23 04:05:03 +0900

    add manual pwa support

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // 최소 서비스워커
+});
